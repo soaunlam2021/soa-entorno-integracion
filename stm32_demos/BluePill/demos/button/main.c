121c0f1e5332c21708af0fd498a8fae4833d8fd0
soaunlam2021 <soaunlam2021@gmail.com>
2021-10-18 18:25:30 +0000

    -se empezo a adaptar el cÃdigo fuente del ejemplo del button para Bluepill para que trabaje con gprof

--- stm32_demos/BluePill/demos/button/main.c
+++ stm32_demos/BluePill/demos/button/main.c
@@ -1,6 +1,10 @@
 #define USE_STDPERIPH_DRIVER
 #include "stm32f10x.h"
 #include "stm32_f103.h"
+#include "profiling/profil.h"
+#include "profiling/gmon.h" /* for _mcleanup() */
+#include <unistd.h> /* for _exit() */
+
 int main(void)
 {
     int last_button_state, new_button_state;
@@ -18,7 +22,9 @@ int main(void)
        {
            if(new_button_state)
            {
-				 GPIO_WriteBit(GPIOC,GPIO_Pin_13,Bit_SET);
+				 //GPIO_WriteBit(GPIOC,GPIO_Pin_13,Bit_SET);
+        	   //_exit(0);
+
            }else
            {
         	   GPIO_WriteBit(GPIOC,GPIO_Pin_13,Bit_RESET);
@@ -27,3 +33,13 @@ int main(void)
        last_button_state = new_button_state;
    }
 }
+
+void _exit(int status)
+{
+  (void)status; /* not used */
+  _mcleanup(); /* write gmon.out file */
+  /* turn on all LED's ==> WHITE */
+  GPIO_WriteBit(GPIOC,GPIO_Pin_13,Bit_SET);
+  for(;;){} /* does not return */
+}
+
